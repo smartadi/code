67a393b0f1cb8ff4f231fa7d0f7c2404b1e4c008
smartadi <adityad@uw.edu>
2023-07-11 22:03:55 -0700

    Tue

--- ground_truth_model/simple_model_control.m
+++ ground_truth_model/simple_model_control.m
@@ -91,12 +91,15 @@ A = randn(2*n,n);
 b = randn(2*n,1);
 c = randn(n,1);
 d = randn;
+
 cvx_begin
    variable x(n)
    dual variables y z
    minimize( c' * x + d )
    subject to
+   for i = 1:1     
       y : A * x <= b;
+   end
 cvx_end
 
 echo off
